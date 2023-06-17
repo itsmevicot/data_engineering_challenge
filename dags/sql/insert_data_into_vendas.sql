f13860bf0e645c84e7897da92f2affc16e5ab599
Victor <victorchagasms@gmail.com>
2023-06-17 17:53:11 -0300

    feat: task e script para a tabela de vendas

---
+++ dags/sql/insert_data_into_vendas.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS public;
+
+
+CREATE TABLE IF NOT EXISTS public.TB_VENDAS (
+    id_venda serial PRIMARY KEY,
+    id_funcionario integer REFERENCES public.TB_FUNCIONARIO (id),
+    id_categoria integer REFERENCES public.TB_CATEGORIA (id),
+    data_venda date,
+    venda integer
+);
+
+{% set rows = ti.xcom_pull(task_ids='task_transform_data_from_postgresql') %}
+
+{% for row in rows.iterrows() %}
+    INSERT INTO public.TB_VENDAS (id_venda, id_funcionario, id_categoria, data_venda, venda)
+    VALUES ({{ row[1]['id_venda'] }}, {{ row[1]['id_funcionario'] }}, {{ row[1]['id_categoria'] }},
+            '{{ row[1]['data_venda'] }}', {{ row[1]['venda'] }})
+    ON CONFLICT (id_venda) DO UPDATE
+    SET id_funcionario = EXCLUDED.id_funcionario,
+        id_categoria = EXCLUDED.id_categoria,
+        data_venda = EXCLUDED.data_venda,
+        venda = EXCLUDED.venda;
+{% endfor %}
